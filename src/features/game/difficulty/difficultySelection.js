ec57d4b22664e071117ae58a8d1ac58ffa1349da
DanDean191 <daniel.dean191@hotmail.com>
2022-09-06 21:21:08 +0100

    added main menu loading

--- src/features/game/difficulty/difficultySelection.js
+++ src/features/game/difficulty/difficultySelection.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setCardPairs, setDifficulty, selectDifficulty } from '../gameSlice.js';
 import './difficultySelection.scss'
 
 const DifficultySelection = () => {
+    const [loading, setLoading] = useState(true)
     const dispatch = useDispatch();
     const customDifficulty = useSelector(selectDifficulty);
 
@@ -12,16 +13,30 @@ const DifficultySelection = () => {
         dispatch(setCardPairs(difficulty))
     }
 
+    useEffect(() => {
+        const changeLoad = () => setLoading(false);
+
+        window.addEventListener('load', changeLoad);
+
+        return () => window.removeEventListener('load', changeLoad)
+    })
+
     return (
-        <div className="difficulty-container">
-            <h2>Difficulty Selection</h2>
-            <button onClick={()=> quickDifficulty(2)}>Easy</button>
-            <button onClick={()=> quickDifficulty(4)}>Medium</button>
-            <button onClick={()=> quickDifficulty(8)}>Hard</button>
-            <h3>Custom Difficulty</h3>
-            <input type="range" min="1" max="10" value={customDifficulty} onChange={(e)=> dispatch(setDifficulty(e.target.value))} />
-            <button onClick={()=> dispatch(setCardPairs(customDifficulty))}>{customDifficulty}</button>
-        </div>
+        <>
+            {
+                loading 
+                ?   <h1>LOADING...</h1> 
+                :   <div className="difficulty-container">
+                        <h2>Difficulty Selection</h2>
+                        <button onClick={()=> quickDifficulty(2)}>Easy</button>
+                        <button onClick={()=> quickDifficulty(4)}>Medium</button>
+                        <button onClick={()=> quickDifficulty(8)}>Hard</button>
+                        <h3>Custom Difficulty</h3>
+                        <input type="range" min="1" max="10" value={customDifficulty} onChange={(e)=> dispatch(setDifficulty(e.target.value))} />
+                        <button onClick={()=> dispatch(setCardPairs(customDifficulty))}>{customDifficulty}</button>
+                    </div>
+            }
+        </>
     )
 }
 
